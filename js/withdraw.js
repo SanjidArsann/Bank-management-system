dd69600801cb5cc6c8503aeee4eb2acd17689471
SanjidArsann <moshiurrahmanm3@gmail.com>
2023-10-08 12:24:50 +0600

    done

--- js/withdraw.js
+++ js/withdraw.js
@@ -3,22 +3,41 @@ document.getElementById('withdraw-btn').addEventListener('click',function(){
     const withdrawAmount  = document.getElementById('withdraw-field');
     let withdrawValueString = withdrawAmount.value;
     const withdrawValue = parseFloat(withdrawValueString);
-    
-    const withdraw = document.getElementById('withdraw');
-    const previousWithdrawString = withdraw.innerText;
-    const previousWithdraw = parseFloat(previousWithdrawString);
 
-    const totalWithdraw = previousWithdraw + withdrawValue;
-    withdraw.innerText = totalWithdraw;
-
-    //Total Balance
     const balanceAmount = document.getElementById('balance');
     const balanceString = balanceAmount.innerText;
     const balance = parseFloat(balanceString);
-    const totalBalance = balance - withdrawValue;
-    balanceAmount.innerText = totalBalance;
 
-    withdrawAmount.value = '';
+    if(withdrawValue <= 0){
+        window.alert('Invalid Amount');
+    }
 
+    // if(withdrawValue < balance){
+    //     window.alert('Invalid Amount');
+    // }
+    
+    if(withdrawValue > 0){
+        const withdraw = document.getElementById('withdraw');
+        const previousWithdrawString = withdraw.innerText;
+        const previousWithdraw = parseFloat(previousWithdrawString);
+    
+        const totalWithdraw = previousWithdraw + withdrawValue;
+        
+    
+        //Total Balance
+        
+        const totalBalance = balance - withdrawValue;
+   if(totalBalance < 0){
+    window.alert('Invalid Amount');
 
+    }   
+    else{
+        balanceAmount.innerText = totalBalance;
+        withdraw.innerText = totalWithdraw;
+    } 
+        
+    
+        withdrawAmount.value = '';
+
+    }
 })
